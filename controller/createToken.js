b2527a689a0a5eab70d5f8de358b34b8a991e181
EarthenLynx <t.quante@gmx.net>
2020-08-31 18:32:58 +0200

    Adjusted create token logic

--- controller/createToken.js
+++ controller/createToken.js
@@ -8,41 +8,62 @@ CREATE_TOKEN = (req, res) => {
   let { userEmail, userPlaintextPassword } = req.body;
 
   // If either of these are missing, send back an error msg
-  if(!userEmail || !userPlaintextPassword) {
-    res.status(400).send({status: "error", msg: "Please type in your password and your username"})
-    return;
-  }
+  if (!userEmail || !userPlaintextPassword) {
+    res.status(400).send({ status: "error", msg: "Please type in your password and your username" });
 
-  // Execute a query on the db
-  user.findOne({ userEmail }, (err, docs) => {
-    // Compare the password's hash with one another
-    bcrypt.compare(userPlaintextPassword, docs.userPassword, (err, result) => {
-      if (result === true) {
-        // Restructure the payload
-        const { userId, userName, userEmail, userRole } = docs;
-        let payload = {userId, userName, userEmail, userRole};
-        
+    // If username and password are provided, move ahead
+  } else {
 
-        // Pass in the options
-        const options = { expiresIn: "15m", audience: userRole, issuer: "jwt-api" };
+    // Execute a query on the db
+    user.findOne({ userEmail }, (err, docs) => {
 
-        // Create the token and sign it
-        const token = jwt.sign(payload, process.env.SECRET, options);
-
-        // Send the token along with a success message
-        res.status(200).send({
-          status: "success",
-          msg: "You've been authenticated and will be redirected in a moment ... ",
-          token
-        });
-      } else {
+      // If no user is found in the db, send an error msg
+      if (!docs) {
         res.status(401).send({
           status: "error",
-          msg: "User - PW combination not found. You might want to sign up first.",
+          msg: "Email adress was not found. A typo, maybe?.",
         });
+
+        // If username is found, move ahead
+      } else {
+        
+        // Compare the password's hash with one another
+        bcrypt.compare(userPlaintextPassword, docs.userPassword, (err, result) => {
+
+          // If passwords do not match, send an error msg
+          if (!result) {
+            res.status(401).send({
+              status: "error",
+              msg: "The password you've typed in is incorrect. Please retry ..."
+            });
+
+            // If passwords do match, create a token
+          } else {
+
+            // Restructure the payload
+            const { userId, userName, userEmail, userRole } = docs;
+            let payload = { userId, userName, userEmail, userRole };
+
+
+            // Pass in the options
+            const options = { expiresIn: "15m", audience: userRole, issuer: "jwt-api" };
+
+            // Create the token and sign it
+            const token = jwt.sign(payload, process.env.SECRET, options);
+
+            // Send the token along with a success message
+            res.status(200).send({
+              status: "success",
+              msg: "You've been authenticated and will be redirected in a moment ... ",
+              token
+            });
+          }
+        })
       }
-    })
-  });
+    });
+  }
+
+
 };
 
 module.exports = CREATE_TOKEN;
