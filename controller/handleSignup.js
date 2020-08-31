b56b767cd48ac3f50e59305999388db64cd1da33
EarthenLynx <t.quante@gmx.net>
2020-08-31 18:46:05 +0200

    DId some minor changes to ui

--- controller/handleSignup.js
+++ controller/handleSignup.js
@@ -11,37 +11,39 @@ const HANDLE_SIGNUP = (req, res) => {
   const { userName, userPlaintextPassword, userEmail, userRole } = data;
 
   // If either of these are missing, send back an error message
-  if(!userName || !userPlaintextPassword || !userEmail || !userRole) {
-    res.status(400).send({status: "error", msg: "Please provide all mandatory information"});
-    return;
-  }
+  if (!userName || !userPlaintextPassword || !userEmail || !userRole) {
+    res.status(400).send({ status: "error", msg: "Please provide all mandatory information" });
 
-  // Check if user's email adressalready signed up
-  User.findOne({ userEmail }, (err, docs) => {
-    // If user exists, send back an error message
-    if (docs) {
-      res.status(400).send({ status: "error", msg: "This email has already been registered, please use another one" });
+    // If all mandatory information are provided, move ahead
+  } else {
+
+    // Check if user's email adressalready signed up
+    User.findOne({ userEmail }, (err, docs) => {
       
-      // If user doesn't exist, create a new one and save it to the DB
-    } else {
+      // If user exists, send back an error message
+      if (docs) {
+        res.status(400).send({ status: "error", msg: "This email has already been registered, please use another one" });
 
-      // Encrypt the user's password
-      bcrypt.hash(userPlaintextPassword, 10, (err, userPassword) => {
-        if (err) throw err;
+        // If user doesn't exist, create a new one and save it to the DB
+      } else {
 
-        // Create new user model and send back a message to the client.
-        let user = new User({ userId, userName, userPassword, userEmail, userRole })
-        user.save(err => {
-          if (err) {
-            res.status(500).send({ status: "error", msg: "User could not be created. Please enter all mandatory fields." });
-          } else {
-            res.status(200).send({ status: "success", msg: "User successfully created" });
-          }
-        })
-      });
-    }
-  })
+        // Encrypt the user's password
+        bcrypt.hash(userPlaintextPassword, 10, (err, userPassword) => {
+          if (err) throw err;
 
+          // Create new user model and send back a message to the client.
+          let user = new User({ userId, userName, userPassword, userEmail, userRole })
+          user.save(err => {
+            if (err) {
+              res.status(500).send({ status: "error", msg: "User could not be created. Please enter all mandatory fields." });
+            } else {
+              res.status(200).send({ status: "success", msg: "User successfully created" });
+            }
+          })
+        });
+      }
+    })
+  }
 };
 
 module.exports = HANDLE_SIGNUP;
